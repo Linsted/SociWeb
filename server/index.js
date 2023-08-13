20c48fb2be164c2b264f2331e9f17d4cf2a58494
Linsted <ivan.malenko18@gmail.com>
2023-08-13 12:13:42 +0300

    add mock data to the DB

--- server/index.js
+++ server/index.js
@@ -11,7 +11,6 @@ import authRoutes from './routes/authRoutes.js';
 import postRoutes from './routes/postRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
