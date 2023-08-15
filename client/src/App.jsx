3c188460dddfbae6c1046914bdebaae13fc1f0e9
Linsted <ivan.malenko18@gmail.com>
2023-08-15 21:44:11 +0300

    add theme Settings

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,28 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import React from 'react';
+import React, { useMemo } from 'react';
+import { useSelector } from 'react-redux';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { themeSettings } from 'theme.js';
 import Login from './pages/Login.jsx';
 import Profile from './pages/Profile.jsx';
 import Home from './pages/Home.jsx';
 
 function App() {
+	const mode = useSelector((state) => state.mode);
+	const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
 	return (
 		<div className="App">
 			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={Login} />
-					<Route path="/home" element={Home} />
-					<Route path="/profile/:userId" element={Profile} />
-				</Routes>
+				<ThemeProvider theme={theme}>
+					<CssBaseline />
+					<Routes>
+						<Route path="/" element={<Login />} />
+						<Route path="/home" element={<Home />} />
+						<Route path="/profile/:userId" element={<Profile />} />
+					</Routes>
+				</ThemeProvider>
 			</BrowserRouter>
 		</div>
 	);
